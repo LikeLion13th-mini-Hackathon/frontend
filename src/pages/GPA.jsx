cd4e53c867f46549152d0a773984a0bb4cffb188
sh2624 <sh121273@gmail.com>
2025-07-06 21:26:07 +0900

    Feat: 메인페이지 API 연동

--- src/pages/GPA.jsx
+++ src/pages/GPA.jsx
@@ -6,18 +6,6 @@ import GpaTable from "../components/gpa/GpaTable";
 import Footer from "../components/Footer";
 import axios from "axios";
 
-const gradeToPoint = {
-  "A+": 4.5,
-  A0: 4.0,
-  "B+": 3.5,
-  B0: 3.0,
-  "C+": 2.5,
-  C0: 2.0,
-  "D+": 1.5,
-  D0: 1.0,
-  F: 0.0,
-};
-
 function Gpa() {
   const [selectedSemester, setSelectedSemester] = useState("1학년 1학기");
   const [allSubjects, setAllSubjects] = useState({});
